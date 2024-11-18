3a36300bb488c2d655b134d858d701985aec8661
David Marin <davidmarin81@gmail.com>
2024-11-18 21:10:22 +0100

    With postgresl docker working

--- src/main/java/com/example/demo/Configuration/CorsConfiguration.java
+++ src/main/java/com/example/demo/Configuration/CorsConfiguration.java
@@ -9,7 +9,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                //.allowedOrigins("http://localhost:5172")
+                .allowedMethods("GET", "POST")
                 .allowedHeaders("*");
     }
 }
